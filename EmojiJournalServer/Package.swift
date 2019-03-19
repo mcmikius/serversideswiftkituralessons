05c765bddee46e6292c0367437684bc413bd3c65
Michail Bondarenko <mcmikius15@gmail.com>
2019-03-19 10:40:43 +0200

    add new dependency

--- EmojiJournalServer/Package.swift
+++ EmojiJournalServer/Package.swift
@@ -7,14 +7,15 @@ let package = Package(
     name: "EmojiJournalServer",
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.6.2"),
-        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.8.0")
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.8.0"),
+        .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "9.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "EmojiJournalServer",
-            dependencies: ["Kitura", "HeliumLogger"]),
+            dependencies: ["Kitura", "HeliumLogger", "CloudEnvironment"]),
         .testTarget(
             name: "EmojiJournalServerTests",
             dependencies: ["EmojiJournalServer"]),
